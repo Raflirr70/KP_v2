b67f403e3695b77f14f321f514becf9d0b1818ec
raflirr70 <raflirr70@gmail.com>
2025-12-10 21:41:21 +0700

    Mengganti Halaman Login, Menjadi Lebih Bagus

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:kerprak/model/user.dart';
 import 'package:provider/provider.dart';
 
@@ -13,65 +14,142 @@ class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _emailCtrl = TextEditingController();
   final TextEditingController _passwordCtrl = TextEditingController();
-  bool _obscure = false;
+  bool _obscure = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  Icon(Icons.person, size: 80, color: Colors.teal),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    controller: _emailCtrl,
-                    decoration: const InputDecoration(
-                      labelText: "Email",
-                      prefixIcon: Icon(Icons.email),
-                    ),
-                    validator: (v) =>
-                        v!.isEmpty ? "Email tidak boleh kosong" : null,
-                  ),
-                  const SizedBox(height: 12),
-                  TextFormField(
-                    controller: _passwordCtrl,
-                    obscureText: _obscure,
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      prefixIcon: const Icon(Icons.lock),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          _obscure ? Icons.visibility : Icons.visibility_off,
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              /// LOGO
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(horizontal: 50),
+                child: Image.asset("lib/asset/rumahGadang.png"),
+              ),
+
+              /// TITLE
+              Text(
+                "Ampera Saiyo",
+                style: GoogleFonts.pacifico(
+                  fontSize: 35,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                "Welcome back!",
+                style: TextStyle(fontSize: 15, color: Colors.grey),
+              ),
+
+              const SizedBox(height: 30),
+
+              /// FORM
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    /// USERNAME FIELD
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextFormField(
+                        controller: _emailCtrl,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return '            Username tidak boleh kosong';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.person_outline),
+                          border: InputBorder.none,
+                          hintText: "Username",
                         ),
-                        onPressed: () => setState(() => _obscure = !_obscure),
                       ),
                     ),
-                    validator: (v) =>
-                        v!.isEmpty ? "Password tidak boleh kosong" : null,
-                  ),
-                  const SizedBox(height: 20),
-                  Consumer<Users>(
-                    builder: (context, value, child) {
-                      return SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            value.Login(_emailCtrl.text, _passwordCtrl.text);
-                          },
-                          child: Text("Masuk"),
+                    const SizedBox(height: 15),
+
+                    /// PASSWORD FIELD
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: TextFormField(
+                        controller: _passwordCtrl,
+                        obscureText: _obscure,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return '            Password tidak boleh kosong';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.lock_outline),
+                          border: InputBorder.none,
+                          hintText: "Password",
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _obscure
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                            ),
+                            onPressed: () =>
+                                setState(() => _obscure = !_obscure),
+                          ),
                         ),
-                      );
-                    },
-                  ),
-                ],
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+
+                    /// SIGN IN BUTTON
+                    Consumer<Users>(
+                      builder: (context, value, child) {
+                        return SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                // Hanya login jika form valid
+                                value.Login(
+                                  _emailCtrl.text,
+                                  _passwordCtrl.text,
+                                );
+                              }
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            child: const Text(
+                              "Sign in",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-            ),
+
+              const SizedBox(height: 20),
+            ],
           ),
         ),
       ),
